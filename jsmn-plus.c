6b2b72afe75a82fa82d0c90771170261850ce99f
John Boyd <johnboydiv@gmail.com>
2017-08-10 17:14:29 -0500

    Added jsmn_search() function

---
+++ jsmn-plus.c
@@ -0,0 +1,59 @@
+#include "jsmn.h"
+#include "jsmn-plus.h"
+#include "string.h"
+
+/**
+ * Search for a token within the scope of specified JSON object token. This is especially
+ * useful for quickly finding nested JSON tokens whithout using excessive resources.
+ */
+int jsmn_search(const char *js, jsmntok_t *token_list, int num_tokens,
+                int scope_object, const char *search_key)
+{
+    int i, token_len, num_remaining, skip_count;
+    jsmntok_t *token;
+
+    // Verify that this is truly an object
+    i = scope_object;
+    if(token_list[i].type != JSMN_OBJECT)
+    {
+        return JSMN_PLUS_ERROR_INVALSCOPE;
+    }
+
+    // Loop through each token in this object
+    skip_count = 0;
+    num_remaining = token_list[i].size;
+    while( (i < num_tokens) && (num_remaining > 0) )
+    {
+        i++; // Increment token index
+        token = &token_list[i];
+        token_len = token->end - token->start;
+
+        // Skip tokens that are not immediately in this JSON object
+        if( (token->type == JSMN_OBJECT) || (token->type == JSMN_ARRAY) )
+        {
+            skip_count += token->size;
+            continue;
+        }
+
+        // Skip tokens that are not in this object scope
+        if(skip_count > 0)
+        {
+            skip_count += token->size - 1;
+            continue;
+        }
+
+        // Update how many tokens remain in this object
+        num_remaining += token->size - 1;
+
+        // Check if this is the token we want
+        if((token_len == strlen(search_key)) &&
+                (memcmp(&js[token->start], search_key, token_len) == 0))
+        {
+            // Success! Return the index of the token associated with this token
+            return i;
+        }
+    }
+
+    // Failure! Key not found
+    return JSMN_PLUS_ERROR_NOTFOUND;
+}
---
+++ jsmn-plus.h
@@ -0,0 +1,29 @@
+#ifndef __JSMN_PLUS_H_
+#define __JSMN_PLUS_H_
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum jsmnpluserr {
+  /* The specified string was not found in the token list */
+  JSMN_PLUS_ERROR_NOTFOUND = JSMN_ERROR_PART + 1,
+  /* Speciffied scope object is not of JSMN_OBJECT type */
+  JSMN_PLUS_ERROR_INVALSCOPE,
+};
+
+
+/*
+ * Search for a token within the scope of specified JSON object token. This is especially
+ * useful for quickly finding nested JSON tokens whithout using excessive resources.
+ */
+int jsmn_search(const char *js, jsmntok_t *token_list, int num_tokens,
+                int scope_object, const char *search_key);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __JSMN_PLUS_H_ */
